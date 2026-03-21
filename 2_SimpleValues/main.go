d24f29572c92307f13ed8bb0f2780925b091ad00
Arindam Pandey <arindampandey63@gmail.com>
2026-03-21 23:03:04 +0530

    Add files via upload

---
+++ 2_SimpleValues/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	//Simple Values
+
+	//int
+	fmt.Println(1)
+	fmt.Println(1 + 1)
+
+	//String
+	fmt.Println("Hello , How are you ??")
+
+	//Bool
+	fmt.Println(true)
+	fmt.Println(false)
+
+	//Float
+	fmt.Println(10.599)
+	fmt.Println(9.0 / 2.0)
+}
