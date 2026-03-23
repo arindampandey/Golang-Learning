b500c258ba68f8e0a3744fc5944c2f75440f2c22
Arindam Pandey <arindampandey63@gmail.com>
2026-03-23 19:02:23 +0530

    Add files via upload

---
+++ 3_variables/variables.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var name string = "Gooooo"
+	// var name = "Gooooo"
+
+	var is_good = true
+	var age = 23
+
+	//Shorthand syntax
+	name := "Hello"
+
+	// var number float32 = 43.66
+	var number = 43.66
+	fmt.Println(number)
+	
+	var num int
+	num = 6
+	fmt.Println(num)
+
+	fmt.Println(name)
+	fmt.Println(is_good)
+	fmt.Println(age)
+}
